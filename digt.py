a7bd983a4061e7ea26f5cd116a88dd9c346f99d8
Vilijam <37106752+Vilijam@users.noreply.github.com>
2019-08-15 13:50:14 +0200

    Commit message

--- digt.py
+++ digt.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-print("En racergrøn gradskala eksplodere tilbageilden, der er vrængende eller halvgammel i hoste, uendeligt, min hårde sjælens tungeminer består, nødvendighed og tunge, tiltagende bestemmelser ser sig i tilstand og skyld, kød, spørgende hoved leveres uden hendes tomme stribe den kontrafaktiske sugende, bortrejst over en gående form og tiltagende efter en rigtig bevidsthed, en nu spørgende status i krop.")
-=======
-print("En grøn gradskala sprænger tilbageilden, som er vrængende ellerhalvgammel i hoste, evigt, min hårde sjælens tungeminer består, nødvendighed og tunge, tiltagende bestemmelser ser sig i tilstand og skyld, kød, bortrejst over en vandrende form og tiltagende efter en rigtig bevidsthed, spørgende hoved leveres uden hendes tomme stribe den kontrafaktiske sugende, en nu spørgende status i krop.")
->>>>>>> 76cce067a5d7ab0b2fc82625ab7097ae72e1acd1
+print("En racergrøn gradskala eksplodere tilbageilden, der er vrængende eller halvgammel i hoste, uendeligt, min hårde sjælens tungeminer består, nødvendighed og tunge, tiltagende bestemmelser ser sig i tilstand og skyld, kød, bortrejst over en gående form og tiltagende efter en rigtig bevidsthed, spørgende hoved leveres uden hendes tomme stribe den kontrafaktiske sugende, en nu spørgende status i krop.")
