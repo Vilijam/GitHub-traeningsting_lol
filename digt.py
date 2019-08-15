20f3a815156ca52ec52c75c66c03c4864e557452
MadsKK <35922365+MadsKK@users.noreply.github.com>
2019-08-15 13:52:21 +0200

    kfskofd

--- digt.py
+++ digt.py
@@ -1,1 +1,6 @@
-print("En racergrøn gradskala eksplodere tilbageilden, der er vrængende eller halvgammel i hoste, uendeligt, min hårde sjælens tungeminer består, nødvendighed og tunge, tiltagende bestemmelser ser sig i tilstand og skyld, kød, bortrejst over en gående form og tiltagende efter en rigtig bevidsthed, spørgende hoved leveres uden hendes tomme stribe den kontrafaktiske sugende, en nu spørgende status i krop.")
+print("""En racergrøn gradskala eksplodere tilbageilden, der er vrængende eller halvgammel i hoste,
+uendeligt, min hårde sjælens tungeminer består, nødvendighed og tunge,
+tiltagende bestemmelser ser sig i tilstand og skyld, kød,
+bortrejst over en gående form og tiltagende efter en rigtig bevidsthed,
+spørgende hoved leveres uden hendes tomme stribe den kontrafaktiske sugende,
+en nu spørgende status i krop.""")
